04fc548c67e8f72dbe78a32955956b2d1cdfa0c9
Franco Catrin <fcatrin@gmail.com>
2020-11-06 17:01:51 -0300

    Add heap example

---
+++ code/bsides2020/05-heap/TestHeap_01.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class TestHeap_01 {
+	static List<int[]> data = new ArrayList<int[]>();
+	
+	private static void loop() throws Exception {
+		while (true) {
+			data.add(buildTestArray());
+			if (Math.random() < 0.2f) Thread.sleep(1);
+			if (Math.random() < 0.5f) System.out.println("size " + data.size());
+		}
+	}
+	
+	private static int [] buildTestArray() {
+		int a[] = new int[256];
+		for(int i=0; i<a.length; i++) {
+			a[i] = (int)Math.random()*1024*1024;
+		}
+		return a;
+	}
+	
+	public static void main(String args[]) throws Exception {
+		loop();
+	}
+}
